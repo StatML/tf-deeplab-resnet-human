ba990cfe23f8b3f31bc069513dbb7f8bc953330d
Engineering-Course <gk12330088@gmail.com>
2017-03-04 16:04:10 +0800

    update utils num classes

--- deeplab_resnet/utils.py
+++ deeplab_resnet/utils.py
@@ -2,18 +2,18 @@ from PIL import Image
 import numpy as np
 import tensorflow as tf
 
-n_classes = 21
+n_classes = 20
 # colour map
 label_colours = [(0,0,0)
-                # 0=background
-                ,(128,0,0),(0,128,0),(128,128,0),(0,0,128),(128,0,128)
-                # 1=aeroplane, 2=bicycle, 3=bird, 4=boat, 5=bottle
-                ,(0,128,128),(128,128,128),(64,0,0),(192,0,0),(64,128,0)
-                # 6=bus, 7=car, 8=cat, 9=chair, 10=cow
-                ,(192,128,0),(64,0,128),(192,0,128),(64,128,128),(192,128,128)
-                # 11=diningtable, 12=dog, 13=horse, 14=motorbike, 15=person
-                ,(0,64,0),(128,64,0),(0,192,0),(128,192,0),(0,64,128)]
-                # 16=potted plant, 17=sheep, 18=sofa, 19=train, 20=tv/monitor
+                # 0=Background
+                ,(128,0,0),(255,0,0),(0,85,0),(170,0,51),(255,85,0)
+                # 1=Hat,  2=Hair,    3=Glove, 4=Sunglasses, 5=UpperClothes
+                ,(0,0,85),(0,119,221),(85,85,0),(0,85,85),(85,51,0)
+                # 6=Dress, 7=Coat, 8=Socks, 9=Pants, 10=Jumpsuits
+                ,(52,86,128),(0,128,0),(0,0,255),(51,170,221),(0,255,255)
+                # 11=Scarf, 12=Skirt, 13=Face, 14=LeftArm, 15=RightArm
+                ,(85,255,170),(170,255,85),(255,255,0),(255,170,0)]
+                # 16=LeftLeg, 17=RightLeg, 18=LeftShoe, 19=RightShoe
 # image mean
 IMG_MEAN = np.array((104.00698793,116.66876762,122.67891434), dtype=np.float32)
     
